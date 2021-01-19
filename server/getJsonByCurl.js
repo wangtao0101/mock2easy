c6d8f2dac265eb18a60d29c70a0d60398d654536
灵亦 <linyi.wt@alibaba-inc.com>
2021-01-19 19:45:39 +0800

    feat: release 1.0.2, fix quota bug

--- server/getJsonByCurl.js
+++ server/getJsonByCurl.js
@@ -2,42 +2,53 @@
  * Created by lihui on 15-1-21.
  */
 
-var child_process = require('child_process');
+var child_process = require("child_process");
 
-
-var obj2StrParams = function(obj){
+var obj2StrParams = function (obj) {
   var param = [];
 
   for (var prop in obj) {
     param.push(prop + "=" + encodeURIComponent(obj[prop]));
   }
 
-  return param.join('&');
-}
+  return param.join("&");
+};
 
+module.exports = function (mock2easy, callback, domain, url, query, cookie) {
+  cookie = cookie.replace(/"/g, "'");
 
-module.exports = function(mock2easy,callback,domain,url,query,cookie){
-
-  try{
-    var sh = ['curl',' ',
+  try {
+    var sh = [
+      "curl",
+      " ",
       domain,
-      url,' ',
-      '--data "'+obj2StrParams(query)+'" ',
-      cookie ? "-H \'Cookie: ":'',cookie,cookie ?"; \'":''
+      url,
+      " ",
+      '--data "' + obj2StrParams(query) + '" ',
+      cookie ? "-H 'Cookie: " : "",
+      cookie,
+      cookie ? "; '" : "",
     ];
-    sh = sh.join('');
+    sh = sh.join("");
     mock2easy.log();
-    mock2easy.log(('+---------------------'+global.language['SERVER-CURL-LOG']+'--------------------------').yellow);
-    mock2easy.log('| '.yellow + sh.green);
-    mock2easy.log('+-------------------------------------------------------'.yellow);
-    child_process.exec(sh,function(error, stdout, stderr){
-      if(error){
+    mock2easy.log(
+      (
+        "+---------------------" +
+        global.language["SERVER-CURL-LOG"] +
+        "--------------------------"
+      ).yellow
+    );
+    mock2easy.log("| ".yellow + sh.green);
+    mock2easy.log(
+      "+-------------------------------------------------------".yellow
+    );
+    child_process.exec(sh, function (error, stdout, stderr) {
+      if (error) {
         return callback(error);
       }
-      callback(null,stdout);
+      callback(null, stdout);
     });
-
-  } catch(error){
+  } catch (error) {
     callback(error);
   }
-}
+};
