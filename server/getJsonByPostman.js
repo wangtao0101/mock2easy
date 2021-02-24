13109b5fa56d2c62fb8adfe92d1fd7c786a621d9
灵亦 <linyi.wt@alibaba-inc.com>
2021-02-24 20:23:24 +0800

    feat: release 1.1.0

--- server/getJsonByPostman.js
+++ server/getJsonByPostman.js
@@ -1,59 +1,60 @@
-/**
- * Created by lihui on 15-11-24.
- */
+var extend = require("node.extend");
 
+var obj2StrParams = function (obj) {
+  var param = [];
+
+  for (var prop in obj) {
+    param.push(prop + "=" + encodeURIComponent(obj[prop]));
+  }
+
+  return param.join("&");
+};
 
 module.exports = function (mock2easy, callback, postman, req) {
   var http;
 
   var options = {
-    host: postman.hostname,
+    host: postman.getDomain(req),
     port: postman.port,
     path: req.originalUrl,
     method: req.method,
     headers: {
-      'Content-Type': 'application/json',
-      'Cache-Control': 'no-cache',
-      'Cookie': postman.cookie
-    }
+      "Content-Type": "application/x-www-form-urlencoded",
+      cookie: postman.Cookie,
+    },
   };
 
-  if(postman.protocol == 'https'){
-    http = require('https');
+  if (postman.protocol == "https") {
+    http = require("https");
     // 如果是https 忽略证书
     options.rejectUnauthorized = false;
-  }else{
-    http = require('http');
+  } else {
+    http = require("http");
   }
 
-  var _write = JSON.stringify(req.body);
+  var _write = obj2StrParams(extend(true, {}, req.body, postman.parameter));
 
-
-  if (req.method == 'POST') {
-    options.headers['Content-Length'] = _write ? _write.length : 0;
+  if (req.method == "POST") {
+    options.headers["Content-Length"] = _write ? _write.length : 0;
   }
 
   var _req = http.request(options, function (_res) {
-
-    var data = '';
-    _res.on('data', function (chunk) {
+    var data = "";
+    _res.on("data", function (chunk) {
       data += chunk;
     });
-    _res.on('end', function () {
+    _res.on("end", function () {
       callback(null, data);
     });
   });
 
-  _req.on('error', function (e) {
+  _req.on("error", function (e) {
     mock2easy.log(e);
   });
 
-
-
-  if (req.method == 'POST' && _write) {
+  if (req.method == "POST" && _write) {
     _req.write(_write);
   }
 
   _req.end();
-
 };
